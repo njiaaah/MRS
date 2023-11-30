1049f6aa898c0b802ef9b8acb7e3f045ba406a2b
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2023-11-30 07:44:39 +0300

    fix for a fix

--- script/header.js
+++ script/header.js
@@ -3,47 +3,55 @@ document.addEventListener('DOMContentLoaded', ()=>{
 //    header desktop menu drop down
 
 const headerChb = document.querySelector('#header-about-list-toggle')
-const ddItems = document.querySelectorAll('.drop-down-item')
+const dropDownUl = document.querySelector('#dropDownUl')
+const listToUpdate = dropDownUl.querySelectorAll('[hidden]')
 
 headerChb.addEventListener('change',()=>{
-    if(headerChb.checked) {
-
-        if(document.startViewTransition()) {
-            document.startViewTransition(()=>{
-                ddItems.forEach(item => {
+    listToUpdate.forEach(item => {
+        if(headerChb.checked) {
+            if(document.startViewTransition()) {
+                document.startViewTransition(()=>{
                     item.removeAttribute('hidden')
-                });
-            }) 
+                })
+            } else item.removeAttribute('hidden')
         } else {
-            ddItems.forEach(item => {
-                item.removeAttribute('hidden')
-            });
-        }
-    
-
-    } else {
-        if(document.startViewTransition()) {
-            document.startViewTransition(()=>{
-                ddItems.forEach(item => {
+            if(document.startViewTransition()) {
+                document.startViewTransition(()=>{
                     item.setAttribute('hidden', '')
-                });
-            })
-        } else {
-            ddItems.forEach(item => {
-                item.setAttribute('hidden', '')
-            });
+                })
+            } else item.setAttribute('hidden', '')
         }
+    });
+})
 
+// hide menus on resize 
+
+const mobileMenuChb = document.querySelector('#mobile-menu-toggle')
+
+window.addEventListener('resize', ()=>{
+    
+    // header drop down list
+    if(window.innerWidth < 920 && headerChb.checked) {
+        listToUpdate.forEach(item=>{
+            item.setAttribute('hidden', '')
+            headerChb.checked = false
+        })
+    }
+
+    // header mobile menu
+    if(window.innerWidth > 800 && mobileMenuChb.checked) {
+        mobileMenuChb.checked = false
     }
 })
 
-// close menu on mouseout
 
 
 
 
 
-   
 
 
 })
+
+
+
