f8e533f88d11964addcb9beea57a4281e0dcb9be
Albotov Timur <kpacota@lenta.ru>
2023-12-04 17:37:41 +0300

    fix

--- script/q-a.js
+++ script/q-a.js
@@ -5,17 +5,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
       const qaToggleBtns = $('.q-a-head');
       const qaAddons = $('.q-a-addon');
-      const qaLeftToggleBtns = $('.q-a-left-toggle-selected');
-      const qaRightToggleBtns = $('.q-a-right-toggle-selected');
       
 
       qaToggleBtns.click(function() {
         const index = qaToggleBtns.index(this);
         qaAddons.eq(index).slideToggle(175);
-        qaLeftToggleBtns.toggleClass('q-a-left-toggle-selected');
-        qaLeftToggleBtns.toggleClass('q-a-left-toggle');
-        qaRightToggleBtns.toggleClass('q-a-right-toggle-selected');
-        qaRightToggleBtns.toggleClass('q-a-right-toggle');
+        $(this).toggleClass('q-a-left-right-toggle-selected');
+        $(this).toggleClass('q-a-left-right-toggle');
       });
 
     }
